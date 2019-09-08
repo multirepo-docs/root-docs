b1a1a36d3e92a783b57dc354a0695e8e9f993731
adibrastegarnia <adib.rastegarnia@gmail.com>
2019-09-08 09:06:58 -0400

    update build_docs.sh

--- build_docs.sh
+++ build_docs.sh
@@ -5,10 +5,9 @@ git config user.email "arastega@purdue.edu"
 git remote add gh-token "https://${GH_TOKEN}@github.com/multirepo-docs/root-docs.git";
 git fetch gh-token && git fetch gh-token gh-pages:gh-pages;
 CURRENT_PATH=$PWD
-cd docs && git submodule add https://github.com/multirepo-docs/repo-docs-1.git
-cd $CURRENT_PATH
-cd docs && git submodule add https://github.com/multirepo-docs/repo-docs-2.git
-cd $CURRENT_PATH
-cd docs && git submodule add https://github.com/multirepo-docs/repo-docs-3.git
+cd docs 
+git submodule add https://github.com/multirepo-docs/repo-docs-1.git
+git submodule add https://github.com/multirepo-docs/repo-docs-2.git
+git submodule add https://github.com/multirepo-docs/repo-docs-3.git
 cd $CURRENT_PATH
 mkdocs gh-deploy -v --clean --remote-name gh-token;
