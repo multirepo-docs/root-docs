ac32444ffb95c3231c9d73207c8210b323c0f6a6
adibrastegarnia <adib.rastegarnia@gmail.com>
2019-09-12 23:06:39 -0700

    remove all folders except docs

--- build_docs.sh
+++ build_docs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+shopt -s extglob
 git config user.name "adibrastegarnia"
 git config user.email "arastega@purdue.edu"
 git remote add gh-token "https://${GH_TOKEN}@github.com/multirepo-docs/root-docs.git";
@@ -7,7 +7,12 @@ git fetch gh-token && git fetch gh-token gh-pages:gh-pages;
 CURRENT_PATH=$PWD
 cd docs 
 git submodule add https://github.com/multirepo-docs/repo-docs-1.git
+cd repo-docs-1 && rm -rf !(docs)
+cd ..
 git submodule add https://github.com/multirepo-docs/repo-docs-2.git
+cd repo-docs-2 && rm -rf !(docs)
+cd ..
 git submodule add https://github.com/multirepo-docs/repo-docs-3.git
+cd repo-docs-3 && rm -rf !(docs)
 cd $CURRENT_PATH
 mkdocs gh-deploy -v --clean --remote-name gh-token;
